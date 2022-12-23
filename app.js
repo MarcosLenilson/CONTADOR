373b56d0239492c0a33d12b072f35ee66e2cd9ca
Marcos Lenilson Alves Duarte <janajesuslelemarcos@gmail.com>
2022-12-23 04:46:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// set inital value to zero
+let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (click) {
+        const styles = click.currentTarget.classList;
+
+        // Condicionais para incrementação (+1) e decrementação (-1)    
+        if (styles.contains("decrease")) {
+            count--; // (-1)
+        } else if (styles.contains("increase")) {
+            count++; // (+1)
+        } else {
+            count = 0; // Reset
+        }
+        // Condicionais para alterar a cor do contador
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#222";
+        }
+        value.textContent = count;
+    });
+});
